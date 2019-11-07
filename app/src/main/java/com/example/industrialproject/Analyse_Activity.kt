dce9c7c593485931c275536450e8ec840c0ae394
Moisan Simon <simon.moisan@etudiant.univ-rennes1.fr>
2019-11-07 10:20:54 +0100

    Add new activity for analyse image with facial recognition

---
+++ app/src/main/java/com/example/industrialproject/Analyse_Activity.kt
@@ -0,0 +1,12 @@
+package com.example.industrialproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Analyse_Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_analyse_)
+    }
+}
