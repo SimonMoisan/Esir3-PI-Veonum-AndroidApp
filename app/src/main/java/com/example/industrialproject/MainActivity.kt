74222c8c666539932011cd4261608aa69ddfae2d
agegout <adrien.gegout@gmail.com>
2019-12-17 17:02:37 +0100

    Added beginning of the loading screen (WIP)

--- app/src/main/java/com/example/industrialproject/MainActivity.kt
+++ app/src/main/java/com/example/industrialproject/MainActivity.kt
@@ -21,6 +21,12 @@ import android.os.Environment.getExternalStoragePublicDirectory
 import android.view.Window
 import android.view.WindowManager
 import java.io.FileOutputStream
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import kotlin.system.exitProcess
+
 
 class MainActivity : AppCompatActivity()
 {
@@ -240,6 +246,10 @@ class MainActivity : AppCompatActivity()
             Log.d("DEBUG", "Action cancelled")
         }
 
+    }
+    override fun onBackPressed() {
+        finishAffinity()
+        System.exit(0)
     }
 
 }
